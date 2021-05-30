763c734a4bb83a58df871737e8749dd67d05e8ab
nguyendokhachieu <nguyendokhachieu@gmail.com>
2021-05-30 14:12:13 +0700

    fix scss

--- src/components/Header/HeaderMobile/Search/index.js
+++ src/components/Header/HeaderMobile/Search/index.js
@@ -1,14 +1,14 @@
 export default function Search() {
   return (
     <div className="nav-search">
-            <form action="#">
-                <input
-                  type="search"
-                  name="search-text"
-                  className="form-control"
-                  placeholder="Tìm kiếm ..."
-                />
-            </form>
-          </div>
+      <form action="#">
+        <input
+          type="search"
+          name="search-text"
+          className="form-control"
+          placeholder="Tìm kiếm ..."
+        />
+      </form>
+    </div>
   );
 }
