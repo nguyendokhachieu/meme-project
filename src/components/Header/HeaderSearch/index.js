59c32a2cc99c7e1d807096fc178ce279531b2039
nguyendokhachieu <nguyendokhachieu@gmail.com>
2021-06-03 18:07:56 +0700

    handle Search input

--- src/components/Header/HeaderSearch/index.js
+++ src/components/Header/HeaderSearch/index.js
@@ -1,18 +1,44 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+
 export default function HeaderNav() {
-    return (
-        <div className="header-search">
-          <form action="#">
-            <label>
-              <input
-                type="search"
-                name="search-text"
-                className="form-control"
-                placeholder="Nhập từ khóa ..."
-              />
-              <i className="icon-search" />
-            </label>
-          </form>
-      </div>
-    );
-  }
+  const history = useHistory();
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    query.trim().length > 0 && history.push("/search?q=" + query);
+  };
+
+  useEffect(() => {
+    let q = window.location.search;
+    if (q.startsWith("?q=")) {
+      q = q.substr(3, q.length);
+      
+      q = decodeURIComponent(q);
   
+      if (q.trim() !== "") {
+        setQuery(q);
+      }
+    }
+  }, []);
+
+  return (
+    <div className="header-search">
+      <form onSubmit={handleSearch}>
+        <label>
+          <input
+            type="search"
+            name="q"
+            className="form-control"
+            placeholder="Tìm kiếm ..."
+            value={ query }
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+        </label>
+      </form>
+    </div>
+  );
+}
