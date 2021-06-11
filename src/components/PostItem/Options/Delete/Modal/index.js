18809a15fd11e28db05ace148e242774fa66dfb0
nguyendokhachieu <nguyendokhachieu@gmail.com>
2021-06-11 18:13:43 +0700

    fix Post item

--- src/components/PostItem/Options/Delete/Modal/index.js
+++ src/components/PostItem/Options/Delete/Modal/index.js
@@ -1,88 +1,91 @@
-import { useEffect, useRef, useState } from "react";
-import { PostService } from "../../../../../services/posts";
-import NotificationCard from "../../../../shared/NotificationCard";
 import "./modal.scss";
 
-export default function Modal({
-    reset = function() {},
-    id
+import { useCallback, useEffect, useRef, useState } from "react";
+import { PostService } from "../../../../../services/posts";
+import { actShowNotificationCard } from "../../../../../store/notifications/actions";
+
+import { useDispatch } from "react-redux";
+
+export default function Modal({ 
+    reset = function () {}, 
+    id 
 }) 
 {
-    const modalRef = useRef();
-    const [hidden, setHidden] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [showNotif, setShowNotif] = useState(false);
-    const [notifContent, setNotifContent] = useState('');
+  const dispatch = useDispatch();
+  const modalRef = useRef();
+  const [hidden, setHidden] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const handleDelete = async () => {
-        setLoading(true);
-        const response = await PostService.delete(id);
+  const handleDelete = async () => {
+    setLoading(true);
+    const response = await PostService.delete(id);
 
-        setLoading(false);
+    setLoading(false);
 
-        if (response.data.deleted) {
-            setHidden(true);
-            setNotifContent('Đã xóa bài viết thành công');
-            setShowNotif(true);
+    if (response.data.deleted) {
+      setHidden(true);
+      dispatch(actShowNotificationCard("Đã xóa bài viết thành công"));
 
-            setTimeout(() => {
-                window.location.reload();
-            }, 1000);
-            
-            return;
-        }
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
 
-        setHidden(true);
-        setNotifContent('Có lỗi xảy ra, vui lòng thử lại');
-        setShowNotif(true);
+      return;
     }
 
-    useEffect(() => {
-        const handle = e => {
-            modalRef.current && !modalRef.current.contains(e.target) && setHidden(true);
-            modalRef.current && !modalRef.current.contains(e.target) && reset(false);
-        }
-        document.addEventListener('mousedown', handle);
+    setHidden(true);
+    dispatch(actShowNotificationCard("Có lỗi xảy ra, vui lòng thử lại"));
+  };
 
-        return () => {
-            document.removeEventListener('mousedown', handle);
-        }
-    });
+  const clickEventCallback = useCallback((e) => {
+    modalRef.current && !modalRef.current.contains(e.target) && setHidden(true);
+    modalRef.current && !modalRef.current.contains(e.target) && reset(false);
+  }, []);
 
-    return (
-        <>
+  useEffect(() => {
+    document.addEventListener("mousedown", clickEventCallback);
+
+    return () => {
+      document.removeEventListener("mousedown", clickEventCallback);
+    };
+  });
+
+  return (
+    <div className={hidden ? "delete-modal-wrap hide" : "delete-modal-wrap"}>
+      <div className="float-modal" ref={modalRef}>
+        <h3 className="modal-title">
             {
-                showNotif
-                    ? (
-                        <NotificationCard 
-                            content={ notifContent } 
-                            showCloseButton={ true } 
-                            show={ showNotif } 
-                        />
-                    )
-                    : 
-                    null
+                loading 
+                    ? <i class="fa fa-spinner fa-spin" style={{ marginRight: "1rem" }}></i> 
+                    : <i class="fad fa-trash-alt icon"></i>
             }
-            <div className={ hidden ? 'delete-modal-wrap hide' : 'delete-modal-wrap' }>
-                <div className="float-modal" ref={ modalRef }>
-                    <h3 className="modal-title">
-                        {
-                            loading 
-                                ? <i class="fa fa-spinner fa-spin" style={{marginRight: '1rem'}}></i>
-                                : <i class="fad fa-trash-alt icon"></i>
-                        }
-                        Xóa bài viết này ?
-                    </h3>
-                    <div className="inner-modal">
-                        <div className="icon-wrap"><i class="fad fa-exclamation-square icon"></i></div>
-                        <h6 className="title">Bài viết sẽ bị xóa vĩnh viễn và không thể khôi phục</h6>
-                        <div className="btn-group">
-                            <button className="delete btn" onClick={ handleDelete }>Xóa</button>
-                            <button className="cancel btn" onClick={ e => { setHidden(true) } }>Hủy bỏ</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+                Xóa bài viết này ?
+        </h3>
+        <div className="inner-modal">
+          <div className="icon-wrap">
+            <i class="fad fa-exclamation-square icon"></i>
+          </div>
+          <h6 className="title">
+            Bài viết sẽ bị xóa vĩnh viễn và không thể khôi phục
+          </h6>
+          <div className="btn-group">
+            <button 
+                className="delete btn" 
+                onClick={handleDelete}
+            >
+              Xóa
+            </button>
+            <button
+              className="cancel btn"
+              onClick={(e) => {
+                setHidden(true);
+              }}
+            >
+              Hủy bỏ
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
